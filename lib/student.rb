66da19b0a9ab8a0bb54e801306eb5612c566e0ed
prahal <vprahaladhan@gmail.com>
2021-05-20 21:21:26 +1000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -3,5 +3,69 @@ require 'active_support/inflector'
 require 'interactive_record.rb'
 
 class Student < InteractiveRecord
+  def self.table_name
+    self.to_s.downcase.pluralize
+  end
 
+  def self.column_names
+    column_names = [] 
+    sql = "PRAGMA table_info('#{table_name}')"
+    DB[:conn].execute(sql).each {|column| column_names << column["name"]}
+    column_names
+  end
+
+  def self.find_by_name(name)
+    students = []
+    DB[:conn].execute("SELECT * FROM #{table_name} WHERE name = '#{name}'").each do |student|
+      students << student.delete_if {|key, value| Float(key) != nil rescue false}
+    end
+    students
+  end
+
+  def self.find_by(search_key)
+    students = []
+    search_str = ''
+
+    search_key.each do |key, value|
+      search_str += "#{key.to_s} = "
+      search_str += ((Float(value) != nil rescue false) ? "#{value}" : "'#{value}'")
+    end
+
+    DB[:conn].execute("SELECT * FROM #{table_name} WHERE #{search_str}").each do |student|
+      students << student.delete_if {|key, value| Float(key) != nil rescue false}
+    end
+    students
+  end
+
+  self.column_names.each do |column|
+    attr_accessor column.to_sym
+  end  
+  
+  def initialize(options = {})
+    options.each {|key, value| self.send("#{key}=", value)}
+  end
+
+  def table_name_for_insert
+    self.class.table_name
+  end
+
+  def col_names_for_insert
+    self.class.column_names.delete_if {|name| name == 'id'}.join(", ")
+  end
+
+  def values_for_insert
+    values = []
+    columns = self.class.column_names.delete_if {|name| name == 'id'}
+    columns.each {|name| values << "'#{self.send(name)}'"}
+    values.join(", ")
+  end
+
+  def save
+    DB[:conn].execute("INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})")
+    self.id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+  end
+
+  def numeric?
+    Float(self) != nil rescue false
+  end
 end
--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -93,7 +93,8 @@ describe Student do
     describe '#save' do
       it 'saves the student to the db' do
         new_student.save
-        expect(DB[:conn].execute("SELECT * FROM students WHERE name = 'Sam'")).to eq([{"id"=>1, "name"=>"Sam", "grade"=>11}])
+        expected_student = {"id"=>1, "name"=>"Sam", "grade"=>11, 0=>1, 1=>"Sam", 2=>11}
+        expect(DB[:conn].execute("SELECT * FROM students WHERE name = 'Sam'")).to eq([expected_student])
       end
 
       it 'sets the student\'s id' do
